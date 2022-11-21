c04e9031120e9d11d8036c2929f0b869dbb0c247
Matt <m.msklee@gmail.com>
2022-11-20 23:31:11 -0500

    Remove Bootstrap for resource card CSS

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -1,20 +1,41 @@
-// import styled from "styled-components";
-import Card from "react-bootstrap/Card";
+import styled from "styled-components";
 
 const ResourceCard = ({ title, link, description, imageUrl }) => {
   return (
-    <Card
-      className="m-3"
-      style={{ width: "18rem" }}
-      onClick={() => window.open(link)}
-    >
-      <Card.Img variant="top" src={`https:${imageUrl}`} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-      </Card.Body>
+    <Card onClick={() => window.open(link)}>
+      <Image src={`https:${imageUrl}`} />
+      <Body>
+        <Title>{title}</Title>
+        <Text>{description}</Text>
+      </Body>
     </Card>
   );
 };
+
+const Card = styled.div`
+  width: 18rem;
+  height: 20rem;
+  border: 1px solid #ececec;
+  background-color: white;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 50%;
+  object-fit: cover;
+`;
+
+const Body = styled.div`
+  margin: 1em;
+`;
+
+const Title = styled.div`
+  color: #333333;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const Text = styled.div`
+  color: #666666;
+`;
 
 export default ResourceCard;
