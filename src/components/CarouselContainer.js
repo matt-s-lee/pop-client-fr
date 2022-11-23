cc1bfb37ff905d87c71fad6abbe47dcd8f0111cb
Matt <m.msklee@gmail.com>
2022-11-23 12:09:34 -0500

    feat: Add image carousel for resources

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -1,10 +1,9 @@
-import React from "react";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import styled from "styled-components";
-import Hero from "./Hero";
 
-const CarouselContainer = ({ language }) => {
+import ResourceCard from "./ResourceCard";
+
+export default function CarouselContainer({ taggedResources, assetDetails }) {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -13,123 +12,67 @@ const CarouselContainer = ({ language }) => {
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 1,
+      partialVisibilityGutter: 200,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 450, min: 0 },
       items: 1,
+      partialVisibilityGutter: 40,
     },
   };
 
   return (
-    <Carousel
-      responsive={responsive}
-      arrows={false}
-      draggable={false}
-      swipeable={false}
-      showDots={true}
-      infinite={true}
-      autoPlay={true}
-      autoPlaySpeed={5000}
-      transitionDuration={5}
-      containerClass="carousel-container"
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
-    >
-      <Hero language={language} />
-
-      {/* <Banner
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1662668862763-dc613ee191ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
-        }}
+    taggedResources &&
+    assetDetails && (
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite={false}
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        partialVisible
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={responsive}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
       >
-        <Row>
-          <TextBox>
-            <Title>
-              <Span>
-                {language === "eng"
-                  ? "Coping with Current Events"
-                  : "Faire face aux événements actuels"}
-              </Span>
-            </Title>
-            <Body>
-              <Span>
-                {language === "eng"
-                  ? "It is possible to have power over your pain."
-                  : "Il est possible d'avoir du pouvoir sur votre douleur."}
-              </Span>
-            </Body>
-          </TextBox>
-        </Row>
-      </Banner>
-      <Banner
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1661846601758-28ce69e3d480?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
-        }}
-      /> */}
-      {/* <Banner
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1662673965352-0fc620737c77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80")`,
-        }}
-      />
-      <Banner
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1662369981337-81c330d439f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")`,
-        }}
-      />
-      <Banner
-        style={{
-          backgroundImage: `url("https://images.unsplash.com/photo-1648334674879-d5f353b518e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2068&q=80")`,
-        }}
-      /> */}
-    </Carousel>
+        {taggedResources.length > 0 &&
+          taggedResources.map((resource) => {
+            return (
+              <ResourceCard
+                key={resource.sys.id}
+                title={resource.fields.title}
+                link={resource.fields.link}
+                description={
+                  resource.fields.descriptionForSmallCard.content[0].content[0]
+                    .value
+                }
+                imageUrl={
+                  assetDetails.find((asset) => {
+                    return asset.sys.id === resource.fields.image.sys.id;
+                  }).fields.file.url
+                }
+              />
+            );
+          })}
+      </Carousel>
+    )
   );
-};
-
-export default CarouselContainer;
-
-const Banner = styled.div`
-  height: 600px;
-  width: 100%;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-`;
-
-const Row = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-
-  & > * {
-    margin: 150px;
-    width: 100%;
-  }
-`;
-
-const TextBox = styled.div`
-  color: white;
-  min-width: 300px;
-  width: 50%;
-
-  & > * {
-    margin: 10px;
-  }
-`;
-
-const Title = styled.div`
-  font-size: 42px;
-  font-weight: 700;
-`;
-
-const Body = styled.div`
-  font-size: 34px;
-  font-weight: 700;
-`;
-
-const Span = styled.span`
-  background-color: green;
-  padding: 5px 15px;
-`;
+}
--- src/components/ResourceCarousel.js
+++ src/components/ResourceCarousel.js
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import ResourceCard from "./ResourceCard";
+import CarouselContainer from "./CarouselContainer";
 
 const ResourceCarousel = ({ resources, tag }) => {
+  const [width, setWidth] = useState(window.innerWidth);
   const resourceDetails = resources.items; // Details for all resources
   const assetDetails = resources.includes.Asset; // Details for all assets, to link to resources
-  const taggedResources = [];
+  const taggedResources = []; // Array of all resources matching the section tag (e.g. typeSleep)
 
+  //------------------------------------------------------------------
+  // Logic to detect if mobile device used (to render image carousel)
+  //------------------------------------------------------------------
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+  let isMobile = width <= 600;
+
+  //-----------------------------------------------------------------------------------
   // If section tag from props (e.g. typeSleep, typeNeuroscience) matches resource tag,
   // push to taggedResources[]
+  //-----------------------------------------------------------------------------------
+
   if (tag) {
     resourceDetails.forEach((resource) => {
       resource.metadata.tags.forEach((sysTag) => {
@@ -20,27 +41,36 @@ const ResourceCarousel = ({ resources, tag }) => {
   }
 
   return (
-    <ResourceWrapper>
-      {taggedResources.length > 0 &&
-        taggedResources.map((resource) => {
-          return (
-            <ResourceCard
-              key={resource.sys.id}
-              title={resource.fields.title}
-              link={resource.fields.link}
-              description={
-                resource.fields.descriptionForSmallCard.content[0].content[0]
-                  .value
-              }
-              imageUrl={
-                assetDetails.find((asset) => {
-                  return asset.sys.id === resource.fields.image.sys.id;
-                }).fields.file.url
-              }
-            />
-          );
-        })}
-    </ResourceWrapper>
+    <>
+      {isMobile ? (
+        <CarouselContainer
+          taggedResources={taggedResources}
+          assetDetails={assetDetails}
+        />
+      ) : (
+        <ResourceWrapper>
+          {taggedResources.length > 0 &&
+            taggedResources.map((resource) => {
+              return (
+                <ResourceCard
+                  key={resource.sys.id}
+                  title={resource.fields.title}
+                  link={resource.fields.link}
+                  description={
+                    resource.fields.descriptionForSmallCard.content[0]
+                      .content[0].value
+                  }
+                  imageUrl={
+                    assetDetails.find((asset) => {
+                      return asset.sys.id === resource.fields.image.sys.id;
+                    }).fields.file.url
+                  }
+                />
+              );
+            })}
+        </ResourceWrapper>
+      )}
+    </>
   );
 };
 
